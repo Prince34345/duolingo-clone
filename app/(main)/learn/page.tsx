6a3a949c404111536cf4bcc9cf6067ad2a94b605
Priyanshu Dhawan <priyanshudhawan55@gmail.com>
2024-04-29 11:36:25 +0530

    queries and loading effect added.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,9 +2,18 @@ import Feedwrapper from "@/components/feedwrapper"
 import Stickywrapper from "@/components/stickywrapper"
 import Header from "./header"
 import UserProgress from "@/components/userprogress"
+import { getUserProgress } from "@/db/queries"
+import { redirect } from "next/navigation"
 
-const LearnPage  = () => {
-    return (
+const LearnPage  = async () => {
+  const userProgressData = getUserProgress();
+  const [userProgress] = await Promise.all([userProgressData])
+  
+  if(!userProgress || !userProgress.activeCourse) {
+    redirect("/courses")
+  }
+  
+  return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
           <Stickywrapper>
              <UserProgress  activeCourse={{title: "Spanish", imageSrc: "/asset/es.svg"}} hearts={5} points={100} hasActiveSubscription={false}/>
